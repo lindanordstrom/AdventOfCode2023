8f5a6bcd97d12f3d8bf0994f1f5c09c80e91a253
Linda <lindanordstrom86@gmail.com>
2023-12-01 11:00:45 +0100

    base setup

---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode2023;
+
+var testInput = new string[] {
+    "two1nine",
+    "eightwothree",
+    "abcone2threexyz",
+    "xtwone3four",
+    "4nineeightseven2",
+    "zoneight234",
+    "7pqrstsixteen",
+};
+
+var realInput = InputLoader.Load(1);
+
+Day1.Part1(realInput);
+Day1.Part2(realInput);
