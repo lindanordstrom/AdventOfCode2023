7f0de71526ab333fbf7e40b2e86c8b69f604ec8d
Linda <lindanordstrom86@gmail.com>
2023-12-01 11:10:34 +0100

    cleanup

--- AdventOfCode2023/Day1.cs
+++ AdventOfCode2023/Day1.cs
@@ -21,31 +21,31 @@ public static class Day1
         var sum = 0;
         foreach (var line in input)
         {
-            var numberArray = NumberArray(line);
-            sum += int.Parse($"{numberArray.First()}{numberArray.Last()}");
+            var charArray = line
+                .AddNumber("0", "zero")
+                .AddNumber("1", "one")
+                .AddNumber("2", "two")
+                .AddNumber("3", "three")
+                .AddNumber("4", "four")
+                .AddNumber("5", "five")
+                .AddNumber("6", "six")
+                .AddNumber("7", "seven")
+                .AddNumber("8", "eight")
+                .AddNumber("9", "nine")
+                .ToCharArray();
+            var first = charArray.First(char.IsDigit);
+            var last = charArray.Last(char.IsDigit);
+
+            sum += int.Parse($"{first}{last}");
         }
         Console.WriteLine(sum);
     }
-
-    private static int[] NumberArray(string line)
-    {
-        line = line.AddNumber("0", "zero").AddNumber("1", "one").AddNumber("2", "two").AddNumber("3", "three").AddNumber("4", "four").AddNumber("5", "five").AddNumber("6", "six").AddNumber("7", "seven").AddNumber("8", "eight").AddNumber("9", "nine");
-        var result = new List<int>();
-        foreach (var character in line)
-        {
-            if (!char.IsDigit(character)) continue;
-            var number = int.Parse(character.ToString());
-            result.Add(number);
-        }
-        
-        return result.ToArray();
-    }
     
-    private static string AddNumber(this string line, string number, string after)
+    private static string AddNumber(this string line, string number, string intoString)
     {
-        if (!line.Contains(after)) return line;
+        if (!line.Contains(intoString)) return line;
 
-        var indexes = line.AllIndexesOf(after).ToList();
+        var indexes = line.AllIndexesOf(intoString).ToList();
         foreach (var index in indexes)
         {
             line = line.Insert(index + 2, number);
@@ -54,7 +54,7 @@ public static class Day1
         return line;
     }
     
-    public static IEnumerable<int> AllIndexesOf(this string str, string searchstring)
+    private static IEnumerable<int> AllIndexesOf(this string str, string searchstring)
     {
         int minIndex = str.IndexOf(searchstring);
         while (minIndex != -1)
