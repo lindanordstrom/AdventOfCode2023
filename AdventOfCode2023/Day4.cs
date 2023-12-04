4c70238df66f33d70a26ee36dd4c6eefdd9594ed
Linda <lindanordstrom86@gmail.com>
2023-12-04 15:04:36 +0100

    Day4: small renamings

--- AdventOfCode2023/Day4.cs
+++ AdventOfCode2023/Day4.cs
@@ -5,7 +5,7 @@ public static class Day4
     public static void Part1(string[] input)
     {
         var sum = 0;
-        input.HandleMatchesPerGame((index, matchesCount) =>
+        input.HandleMatchesPerGame((_, matchesCount) =>
         {
             var points = 0;
             Enumerable.Range(0, matchesCount)
@@ -19,18 +19,18 @@ public static class Day4
     public static void Part2(string[] input)
     {
         var cardCounts = Enumerable.Repeat(1, input.Length).ToArray();
-        input.HandleMatchesPerGame((index, matchesCount) =>
+        input.HandleMatchesPerGame((cardIndex, matchesCount) =>
         {
             Enumerable.Range(0, matchesCount)
                 .ToList()
-                .ForEach(i => cardCounts[index + i + 1] += cardCounts[index]);
+                .ForEach(i => cardCounts[cardIndex + i + 1] += cardCounts[cardIndex]);
         });
         Console.WriteLine(cardCounts.Sum() == 5539496);
     }
 
     private static void HandleMatchesPerGame(this string[] input, Action<int, int> closure)
     {
-        foreach (var (line, index) in input.WithIndex())
+        foreach (var (line, cardIndex) in input.WithIndex())
         {
             var card = line
                 .Remove(0, line.IndexOf(":") + 2)
@@ -39,7 +39,7 @@ public static class Day4
             var ownNumbers = card[1].Split(" ");
             var matchesCount = winningNumbers.Intersect(ownNumbers).Count(num => num != "");
             
-            closure(index, matchesCount);
+            closure(cardIndex, matchesCount);
         }
     }
 }
