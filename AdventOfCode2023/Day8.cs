616ba94cddc3aaca4fbd6df1da09015a840f0314
Linda <lindanordstrom86@gmail.com>
2023-12-09 23:28:16 +0100

    cleanup

--- AdventOfCode2023/Day8.cs
+++ AdventOfCode2023/Day8.cs
@@ -1,5 +1,4 @@
 using System.Numerics;
-using System.Runtime.InteropServices.JavaScript;
 
 namespace AdventOfCode2023;
 
@@ -11,44 +10,37 @@ public static class Day8
     private static void StepsNeeded(this string[] input, string start, string goal)
     {
         var orderIndex = 0;
-        var resetCount = 0;
+        var steps = 0;
+        var stepsPerNode = new List<BigInteger>();
         var order = input.First();
-        var network = input.Last()
+        var nodesMapping = input.Last()
             .Split("\n")
             .ToDictionary(line => line[..3], line => new[] { line[7..10], line[12..15] });
-        var currentNodes = network
+        var currentNodes = nodesMapping
             .Where(entry => entry.Key.EndsWith(start))
             .Select(entry => entry.Key)
             .ToList();
-        var resetTimes = new List<BigInteger>();
         
         while (true)
         {
             foreach (var (currentNode, index) in currentNodes.ToList().WithIndex())
             {
                 var nextNode = order.ToCharArray()[orderIndex] == 'L' ? 0 : 1;
-                currentNodes[index] = network[currentNode][nextNode];
+                currentNodes[index] = nodesMapping[currentNode][nextNode];
             }
-            
-            var shouldResetOrder = orderIndex >= order.Length - 1;
-            resetCount = shouldResetOrder ? resetCount + 1 : resetCount;
-            orderIndex = shouldResetOrder ? 0 : orderIndex + 1;
-
-            if (currentNodes.Any(n => n.EndsWith(goal)))
+            steps++;
+            orderIndex = orderIndex >= order.Length - 1 ? 0 : orderIndex + 1;
+            currentNodes.RemoveAll(n =>
             {
-                currentNodes
-                    .Where(n => n.EndsWith(goal))
-                    .ToList()
-                    .ForEach(node => resetTimes.Add(resetCount));
-                currentNodes.RemoveAll(n => n.EndsWith(goal));
-            }
+                if (!n.EndsWith(goal)) return false;
+                stepsPerNode.Add(steps);
+                return true;
+
+            });
             
             if (currentNodes.Count == 0 || currentNodes.All(n => n.EndsWith(goal))) break;
         }
-
-        var lcmOfResetTimes = resetTimes.Aggregate((a, b) => LowestCommonMultiple(a, b));
-        
-        Console.WriteLine(lcmOfResetTimes * order.Length);
+        Console.WriteLine(stepsPerNode.Aggregate(LowestCommonMultiple));
     }
 
     private static BigInteger LowestCommonMultiple(BigInteger a, BigInteger b)
